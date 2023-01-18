0e56bef85b11185c43ea19822f8062c8b62b3db7
phivuong <phivuong27@gmail.com>
2023-01-19 03:18:34 +0700

    Complete functions

--- Etherscan/Controllers/HomeController.cs
+++ Etherscan/Controllers/HomeController.cs
@@ -1,21 +1,47 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Etherscan.Models;
+using Etherscan.Services;
+using X.PagedList;
+using Etherscan.Models.TokenVM;
 
 namespace Etherscan.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private ITokenService _tokenService;
+    private int PageSize = 10;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ITokenService tokenService)
     {
-        _logger = logger;
+        _tokenService = tokenService;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(int? p)
     {
-        return View();
+        List<TokenTableVM> tableVMs = new List<TokenTableVM>();
+        var tokens = _tokenService.GetAll().OrderByDescending(x => x.TotalSupply);
+        var sumSupply = tokens.Sum(x => x.TotalSupply);
+        int rank = 1;
+        foreach (var token in tokens)
+        {
+            tableVMs.Add(new TokenTableVM()
+            {
+                Id = token.Id,
+                Rank = rank,
+                Symbol = token.Symbol,
+                Name = token.Name,
+                ContractAddress = token.ContractAddress,
+                TotalHolders = token.TotalHolders,
+                TotalSuppyStr = token.TotalSupply.ToString("N0").Replace(".", ""),
+                TotalSupplyPercent = (token.TotalSupply / sumSupply) * 100
+            });
+            rank++;
+        }
+        int pageNumber = (p ?? 1);
+        var tokensPaging = tableVMs.ToPagedList(pageNumber, PageSize);
+        ModelState.Clear();
+        return View(tokensPaging);
     }
 
     public IActionResult Privacy()
--- Etherscan/Program.cs
+++ Etherscan/Program.cs
@@ -1,4 +1,31 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Text.Json.Serialization;
+using Etherscan.Helpers;
+using Etherscan.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+{
+    var services = builder.Services;
+    var env = builder.Environment;
+
+    services.AddDbContext<DataContext>();
+    services.AddCors();
+    services.AddControllers().AddJsonOptions(x =>
+    {
+        // serialize enums as strings in api responses (e.g. Role)
+        x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+        // ignore omitted parameters on models to enable optional params (e.g. User update)
+        x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+    services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+    // configure DI for application services
+    services.AddScoped<ITokenService, TokenService>();
+
+    //services.AddControllersWithViews().AddRazorRuntimeCompilation();
+}
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -19,6 +46,15 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+// global cors policy
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+// global error handler
+app.UseMiddleware<ErrorHandlerMiddleware>();
 
 app.MapControllerRoute(
     name: "default",
